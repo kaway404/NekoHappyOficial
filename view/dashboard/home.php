be20dd733b1fa021a7592742f8216e39850fb933
unknown <xande1231231@hotmail.com>
2018-08-12 18:21:26 -0300

    Dashboard WIP

---
+++ view/dashboard/home.php
@@ -0,0 +1,45 @@
+<div class="align">
+<div class="flex">
+<div class="feed">
+<div class="post">
+
+</div>
+<div class="post">
+
+</div>
+<div class="post">
+
+</div>
+<div class="post">
+
+</div>
+<div class="post">
+
+</div>
+<div class="post">
+
+</div>
+<div class="post">
+
+</div>
+<div class="post">
+
+</div>
+<div class="post">
+
+</div>
+<div class="post">
+
+</div>
+</div>
+
+<div class="status">
+<a href="/profile/<?php echo $user['id'];?>">
+<img class="avatar" src="/assets/img/user/<?php echo $user['avatar'];?>"/>
+<p class="user"><?php echo $user['usuario'];?></p>
+</a>
+<hr style="position: absolute; top: 40px">
+</div>
+
+</div>
+</div>
--- view/dashboard/menu.php
+++ view/dashboard/menu.php
@@ -2,7 +2,7 @@
 	<div class="align">
 		<img class="logo" src="https://vignette.wikia.nocookie.net/aphmau/images/a/ad/Neko_atsume-pumpkin.png/revision/latest?cb=20160510065501"/>
 	<div class="boxcenter">
-		<input class="busca" type="text" placeholder="Busca"/>
+		<input class="busca" type="text" placeholder="Buscar amigos"/>
 	</div>
 	</div>
 </div>
