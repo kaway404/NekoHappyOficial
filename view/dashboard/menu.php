a55bfc16e4816250ba23f7a38a3a4f9d7357d16b
unknown <xande1231231@hotmail.com>
2018-08-12 18:02:24 -0300

    Home WIP

---
+++ view/dashboard/menu.php
@@ -0,0 +1,8 @@
+<div class="header">
+	<div class="align">
+		<img class="logo" src="https://vignette.wikia.nocookie.net/aphmau/images/a/ad/Neko_atsume-pumpkin.png/revision/latest?cb=20160510065501"/>
+	<div class="boxcenter">
+		<input class="busca" type="text" placeholder="Busca"/>
+	</div>
+	</div>
+</div>
