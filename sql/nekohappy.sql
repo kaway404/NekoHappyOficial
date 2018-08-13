73043667a14e23c1ad8fb152ec6c89cd71313876
unknown <xande1231231@hotmail.com>
2018-08-13 18:36:28 -0300

    Fix

--- sql/nekohappy.sql
+++ sql/nekohappy.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 13-Ago-2018 às 21:19
+-- Generation Time: 13-Ago-2018 às 23:36
 -- Versão do servidor: 10.1.34-MariaDB
 -- PHP Version: 7.2.8
 
@@ -44,11 +44,11 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`id`, `cry`, `email`, `password`, `usuario`, `admin`, `avatar`) VALUES
 (10, 'nekohapy_85d9235d10cf197950d843e56b47021ff02b0cee', 'kaway@hotmail.com', 'a0b48bf6735b085374fa984535372a8025210e45', 'kaway', 0, 'default.jpg'),
-(11, 'nekohapy_70909c345cd056c3d4dcd0f5a056fb33e88aaa23', 'anelise@hotmail.com', 'e718576aa3e35cdc17a72ac4d6a7bb2772f5fc32', 'anelise', 0, 'default.jpg'),
-(12, 'nekohapy_800f7e501a3f1a0444a94b324e046b2859f9fb44', 'ininja212@gmail.com', '705314e475103d30f98e4457435b746d7181601f', 'SukyT', 0, 'default.jpg'),
-(13, 'nekohapy_c3da8be1a37068d5cda6fdea08af092ed13417e3', 'luizfelipe34@hotmail.com.br', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 'Me verifica', 0, 'default.jpg'),
-(14, 'nekohapy_2c7eb72c3889dc7409783fb5cd46b574bc775848', 'schirlei@hotmail.com', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 'schirlei', 0, 'default.jpg'),
-(15, 'nekohapy_3883e02b80831d294453b441ef68b4c5db90f9a3', 'anelise51@hotmail.com', '3c555211cad6a0bd179cd6baa104f5bb9beb88e1', 'anelise51', 0, 'default.jpg');
+(11, 'nekohapy_70909c345cd056c3d4dcd0f5a056fb33e88aaa23', 'anelise@hotmail.com', 'e718576aa3e35cdc17a72ac4d6a7bb2772f5fc32', 'anelise', 0, '1.jpg'),
+(12, 'nekohapy_800f7e501a3f1a0444a94b324e046b2859f9fb44', 'ininja212@gmail.com', '705314e475103d30f98e4457435b746d7181601f', 'SukyT', 0, '2.png'),
+(13, 'nekohapy_c3da8be1a37068d5cda6fdea08af092ed13417e3', 'luizfelipe34@hotmail.com.br', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 'Me verifica', 0, '3.jpg'),
+(14, 'nekohapy_2c7eb72c3889dc7409783fb5cd46b574bc775848', 'schirlei@hotmail.com', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 'schirlei', 0, '4.jpg'),
+(15, 'nekohapy_3883e02b80831d294453b441ef68b4c5db90f9a3', 'anelise51@hotmail.com', '3c555211cad6a0bd179cd6baa104f5bb9beb88e1', 'anelise51', 0, '2.png');
 
 --
 -- Indexes for dumped tables
