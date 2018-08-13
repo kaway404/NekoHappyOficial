c7e7ebf02cabf11b6e1d05258c8994aa713b1c7d
unknown <xande1231231@hotmail.com>
2018-08-13 18:34:14 -0300

    Dashboard WIP

--- view/dashboard/home.php
+++ view/dashboard/home.php
@@ -14,6 +14,29 @@ if(isset($_COOKIE['iduser']) && (isset($_COOKIE['cry']) )){?>
 	<p>Seja bem vindo(a) ao NekoHappy</p>
 </div>
 
+
+<div class="uk-position-relative uk-visible-toggle uk-light" uk-slider id="sugeridos">
+<span>Amigos sugeridos</span>
+    <ul class="uk-slider-items uk-child-width-1-2 uk-child-width-1-3@s uk-child-width-1-4@m">
+    	<?php
+    	$iduser = $user['id'];
+    	$result_people = "SELECT * FROM user WHERE id <> '$iduser' LIMIT 40";
+	    $resultado_people = mysqli_query($conn, $result_people);
+	    $people = mysqli_fetch_assoc($resultado_people);
+    	foreach ($resultado_people as $resultado_people => $resultado_peoples) {
+    	?>
+        <li uk-tooltip="<?php echo $resultado_peoples['usuario'];?>">
+            <img src="/assets/img/user/<?php echo $resultado_peoples['avatar'];?>" alt="">
+            <div class="uk-position-center uk-panel"></div>
+        </li>
+    <?php } ?>
+    </ul>
+
+    <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slider-item="previous"></a>
+    <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slider-item="next"></a>
+
+</div>
+
 <div class="post">
 
 </div>
