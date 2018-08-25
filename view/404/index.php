dac6b4762f3303e81bca7cf5b36f6983b53b66ac
unknown <xande1231231@hotmail.com>
2018-08-25 12:17:36 -0300

    Fix

--- view/404/index.php
+++ view/404/index.php
@@ -33,3 +33,25 @@
 </html>
 
 
+<?php
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$dbname = "nekohappy";
+$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
+    
+if(!$conn){
+        echo '';
+}
+else{
+    	$result_back = "SELECT * FROM background WHERE id and ativo = 1 ORDER BY RAND() LIMIT 1";
+	    $result_backed = mysqli_query($conn, $result_back);
+	    $background = mysqli_fetch_assoc($result_backed);
+    	foreach ($result_backed as $result_backed => $result_backeds) {
+  ?>
+  <style type="text/css">
+  	.main .back{
+	background-image: url(/assets/img/background/<?php echo $result_backeds['url'];?>) !important;
+}
+  </style>
+  <?php }} ?>
