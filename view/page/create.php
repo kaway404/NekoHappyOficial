fb141d5e78c1954232c989881c2546ef7a9d3981
unknown <xande1231231@hotmail.com>
2018-08-29 15:12:10 -0300

    Fix

--- view/page/create.php
+++ view/page/create.php
@@ -3,6 +3,7 @@
 if($user['tuturial'] == 0){
 	require './view/dashboard/tuturial/index.php';
 }
+echo '<div class="comunidadenew">';
 echo '<div class="align">';
 echo '<div class="flex">';
 echo '<div class="feed">';
@@ -10,14 +11,10 @@ echo '<div class="feed">';
 echo '<div class="oksrs" style="top: 0px;">';
 require './view/page/criando.php';
 echo '</div>';
-
+echo '</div>';
 // Status
 require './view/dashboard/status/me.php';
 echo '</div>';
 echo '</div>';
-
-echo '<div class="newcomunidadet">
-<div class="back"><div class="after"></div></div>
-</div>';
 
  } else{ include '../404/index.php';}?>
--- view/page/criando.php
+++ view/page/criando.php
@@ -1,15 +1,15 @@
 <?php if(isset($template)){?>
 <div class="newcomunidade">
-<h1 style="color: #fff !important; font-size: 1.4em;">Criando uma comunidade!!</h1>
-<p style="color: #fff !important; font-size: 1.1em;">Crie uma comunidade do seu jogo/filme/série/banda/anime favorito.</p>
+<h1 style="color: #476323 !important; font-size: 1.4em;">Criando uma comunidade!!</h1>
+<p style="color: #476323 !important; font-size: 1.1em;">Crie uma comunidade do seu jogo/filme/série/banda/anime favorito.</p>
 
 <hr>
 
-<p style="color: #fff !important;">Qual vai ser o nome da página?</p>
-<input type="" id="game" name="" placeholder="Exemplo: Sociedade Harry Potter">
+<p style="color: #476323 !important;">Qual vai ser o nome da página?</p>
+<input type="" id="nome" name="" placeholder="Exemplo: Sociedade Harry Potter">
 <hr>
-<p style="color: #fff !important;">Sobre oque é essa comunidade?</p>
-<input type="" id="game" name="" placeholder="Exemplo: Fans de Harry Potter">
+<p style="color: #476323 !important;">Sobre oque é essa comunidade?</p>
+<input type="" id="sobre" name="" placeholder="Exemplo: Fans de Harry Potter">
 <hr>
 <button>Criar comunidade</button>
 
