8cf88fc7120cfacae0acdafdfed13ad4adf388ef
unknown <xande1231231@hotmail.com>
2018-08-31 15:43:47 -0300

    Fix

--- view/dashboard/menu.php
+++ view/dashboard/menu.php
@@ -24,5 +24,45 @@
 	</div>
 </div>
 
+<?php
+$expatual = $user['exp'] * 10;
+$exp = $user['exp'];
+?>
+
+<div class="bottomh">
+	<div class="align">
+		<div class="profile">
+			<div class="exp" uk-tooltip="Progresso">
+				<div uk-tooltip="EXP atual <?Php echo $exp;?> de 10" class="atual"></div>
+			</div>
+			<div class="avatar">
+			</div>
+			<p><?php echo $user['usuario'];?></p>
+		</div>
+	</div>
+</div>
+
+
+<style type="text/css">
+	.bottomh .profile .avatar{
+		background-image: url("/img/user/<?php echo $user['avatar'];?>")
+	}
+
+	.exp{
+		width: 100%;
+		height: 10px;
+		background: #57772c;
+		position: absolute;
+		top: -10px;
+	}
+
+	.exp .atual{
+		width: <?php echo $expatual;?>%;
+		background: #f4f5f4;
+		height: 100%;
+		box-shadow: 1px 1px 1px rgba(0,0,0,0.50);
+	}
+</style>
+
 <?Php } ?>
 <?Php }  else{ include '../404/index.php';}?>
