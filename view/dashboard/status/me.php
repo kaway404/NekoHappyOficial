526ae2b20c92a2202de29a85de054ad1d51005ee
unknown <xande1231231@hotmail.com>
2018-09-02 10:59:21 -0300

    Fix

--- view/dashboard/status/me.php
+++ view/dashboard/status/me.php
@@ -2,7 +2,6 @@
 
 <div class="links">
 <p>Atalhos</p>	
-<center>
 <a href="/create_page">Nova comunidade</a>
 <br>
 <a href="/profile/<?php echo $user['id'];?>">Meu perfil</a>
@@ -10,7 +9,6 @@
 <a href="/toprank">Top rank</a>
 <br>
 <a href="/loja">Loja</a>
-</center>
 </div>
 
 <div class="status">
