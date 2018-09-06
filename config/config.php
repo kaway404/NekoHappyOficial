b30464387b5a741da3fe77906cf08e87e322ebb1
unknown <xande1231231@hotmail.com>
2018-09-06 14:46:59 -0300

    Fix

--- config/config.php
+++ config/config.php
@@ -15,15 +15,10 @@ $dataAtual = date('d/m/Y H:i:s');
 
 // Conexão com banco de dados
 
-// $servidor = "localhost";
-// $usuario = "root";
-// $senha = "";
-// $dbname = "nekohappy";
-
-$servidor = "sql10.freemysqlhosting.net";
-$usuario = "sql10254218";
-$senha = "tD77bquKUT";
-$dbname = "sql10254218";
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$dbname = "nekohappy";
 
 $conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
     
