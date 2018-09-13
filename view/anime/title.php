2bc87fb68a36f1d3ef8bdef72db803f799767b04
unknown <xande1231231@hotmail.com>
2018-09-13 15:37:48 -0300

    Fix

--- view/anime/title.php
+++ view/anime/title.php
@@ -4,7 +4,7 @@ echo '<div class="align">';
 echo '<div class="flex">';
 echo '<div class="feed">';
 echo '<div id="perfil">';
-
+if(isset($anime)){
 echo '<h1 style="font-size: 1.6em;">' . 'Anime ' .$anime['slug'] . '</h1>';
 echo '<p>Genero: ' . $anime['age_rating_guide']. ' |';
 echo ' Rating: ' . $anime['age_rating'] . ' | ' ;
@@ -12,6 +12,12 @@ echo 'Episodios: ' . $anime['episode_count'] . '</p>';
 echo '<p>Sinopse: ' . $anime['synopsis'];
 echo '<p>Trailer</p>';
 echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/' . $anime['youtube_video_id'] . '"' . ' frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
+}
+else{
+	echo '<div class="notice" style="top: 0;">
+    <p>Este anime não existe.</p>
+</div>';
+}
 
 
 echo '</div>';
