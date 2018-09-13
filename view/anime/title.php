b863573fed34d8c9994ac79189c26f1e110f09cf
unknown <xande1231231@hotmail.com>
2018-09-13 15:40:55 -0300

    Fix

--- view/anime/title.php
+++ view/anime/title.php
@@ -7,8 +7,7 @@ echo '<div id="perfil">';
 if(isset($anime)){
 echo '<h1 style="font-size: 1.6em;">' . 'Anime ' .$anime['slug'] . '</h1>';
 echo '<p>Genero: ' . $anime['age_rating_guide']. ' |';
-echo ' Rating: ' . $anime['age_rating'] . ' | ' ;
-echo 'Episodios: ' . $anime['episode_count'] . '</p>';
+echo ' Rating: ' . $anime['age_rating'] . '</p>' ;
 echo '<p>Sinopse: ' . $anime['synopsis'];
 echo '<p>Trailer</p>';
 echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/' . $anime['youtube_video_id'] . '"' . ' frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
